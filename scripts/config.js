46daf717d355f5020b9e956a7b19b6eb45dc50fb
enhaflixx <enhaflixx@gmail.com>
2026-03-24 13:24:40 -0700

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -3238,6 +3238,12 @@ const categoriesConfig = {
             thumbnail: "images/thumbnails/anime-dm.jpg",
             description: "Dark Moon Anime 2026"
         },
+        { 
+            name: "Dark Moon EP.10", 
+            url: "https://kisskh.co/Drama/DARK-MOON--THE-BLOOD-ALTAR---Dark-Moon---Tsuki-no-Saidan/Episode-11?id=12271&ep=208996&page=0&pageSize=100", 
+            thumbnail: "images/thumbnails/anime-dm.jpg",
+            description: "Dark Moon Anime 2026"
+        },
         // ADD OTHER CONTENT HERE
     ]
 },
