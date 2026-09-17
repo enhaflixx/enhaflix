e966233f40485ec0a7f4ffa2f5d1d94d8941b112
enhaflixx <enhaflixx@gmail.com>
2026-09-17 12:57:40 -0700

    Update evan-config.js

--- scripts/evan-config.js
+++ scripts/evan-config.js
@@ -3,7 +3,7 @@
 
 const categoriesConfig = {
     "EVAN Music": {
-        "Discography": [
+        "Singles": [
             { 
                 name: "RIDE OR DIE", 
                 url: "https://open.spotify.com/album/7BUNjTAZNdAbZ3GEhv7F25?si=VdlSc6uZTUix5omYtABKww", 
@@ -17,13 +17,31 @@ const categoriesConfig = {
                 description: "2026 . Single"
             },
         ],
+
+      "Album": [
+{ 
+                name: "DEATH OF ME", 
+                url: "https://open.spotify.com/album/5hYH4zxmh1ePXZaxFyj9wB?si=y6FGRnKYQA-GOS7Rbk1Olw", 
+                thumbnail: "images/evan/thumbnails/evan-album.jpg",
+                description: "2026 . Album"
+            },
+          ],
+
+
         "Music Videos": [
             { 
                 name: "EVAN - Ride or Die MV", 
                 url: "https://www.youtube.com/watch?v=CsGENgLpyaw", 
                 thumbnail: "images/evan/thumbnails/evan-mv-1.jpg",
                 description: "2026 . Music Video"
-            }
+            },
+ { 
+                name: "EVAN - Death of Me MV", 
+                url: "https://www.youtube.com/watch?v=iNMvUYjaieA", 
+                thumbnail: "images/evan/thumbnails/evan-mv-2.jpg",
+                description: "2026 . Music Video"
+            },
+
         ]
     },
 
@@ -35,6 +53,13 @@ const categoriesConfig = {
                 thumbnail: "images/evan/thumbnails/dance-1.jpg",
                 description: "Dance Practice"
             },
+{ 
+                name: "EVAN Death of Me Dance Practice", 
+                url: "https://www.youtube.com/watch?v=2LArv4SztL8", 
+                thumbnail: "images/evan/thumbnails/dance-2.jpg",
+                description: "Dance Practice"
+            },
+
         ],
         },
 
@@ -51,12 +76,108 @@ const categoriesConfig = {
                 url: "https://www.youtube.com/watch?v=sS9XGAFoRM0", 
                 thumbnail: "images/evan/thumbnails/evan-archive-2.jpg",
                 description: "Archive"
-            }
+            },
+{ 
+                name: "EVAN Ride or Die Challenge Behind the Scenes", 
+                url: "https://www.youtube.com/watch?v=yEV-DNKmwuY", 
+                thumbnail: "images/evan/thumbnails/evan-archive-3.jpg",
+                description: "Archive"
+            },
+{ 
+                name: "EVAN Ride or Die Music Shows Behind the Scenes", 
+                url: "https://www.youtube.com/watch?v=eyCjCFY_nlQ", 
+                thumbnail: "images/evan/thumbnails/evan-archive-4.jpg",
+                description: "Archive"
+            },
+{ 
+                name: "EVAN Behind the Scenes Close Friend Radio Show DJ", 
+                url: "https://www.youtube.com/watch?v=8tTzR8oViEg", 
+                thumbnail: "images/evan/thumbnails/evan-archive-5.jpg",
+                description: "Archive"
+            },
+{ 
+                name: "EVAN DEATH OF ME Concept Photo Shoot Sketch", 
+                url: "https://www.youtube.com/watch?v=dZk2pofQFuM", 
+                thumbnail: "images/evan/thumbnails/evan-archive-6.jpg",
+                description: "Archive"
+            },
+{ 
+                name: "EVAN DEATH OF ME MV Shoot Sketch", 
+                url: "https://www.youtube.com/watch?v=wdWFOyxlkks", 
+                thumbnail: "images/evan/thumbnails/evan-archive-7.jpg",
+                description: "Archive"
+            },
 
         ],
         },
 
-    "Debut Documentary": {
+"Sid:E": {
+        "Sid:E": [
+            { 
+                name: "My First Time Setting Up a Workspace", 
+                url: "https://www.youtube.com/watch?v=0U6sXMUYtRM", 
+                thumbnail: "images/evan/thumbnails/evan-side-1.jpg",
+                description: "Sid:E"
+            },
+{ 
+                name: "From the Han River to Busan for the performance", 
+                url: "https://www.youtube.com/watch?v=TB1bu9TxJzQ", 
+                thumbnail: "images/evan/thumbnails/evan-side-2.jpg",
+                description: "Sid:E log"
+            },
+{ 
+                name: "Ride or Die The activity has been completed", 
+                url: "https://www.youtube.com/watch?v=bHELiN_9pCY", 
+                thumbnail: "images/evan/thumbnails/evan-side-3.jpg",
+                description: "Sid:E"
+            },
+{ 
+                name: "The Tokyo mini tour is fun! Haha (+dance workshop)", 
+                url: "https://www.youtube.com/watch?v=Sy28D2DXEdQ", 
+                thumbnail: "images/evan/thumbnails/evan-side-4.jpg",
+                description: "Sid:E log"
+            },
+{ 
+                name: "What should Evan do today? The Evan Special and hide and seek", 
+                url: "https://www.youtube.com/watch?v=TruS_QursBw", 
+                thumbnail: "images/evan/thumbnails/evan-side-5.jpg",
+                description: "Sid:E"
+            },
+{ 
+                name: "[Open Recruitment] Signing up for the Slam Dunk meetup 🏀 First gathering", 
+                url: "https://www.youtube.com/watch?v=aEVD0PRa23Y", 
+                thumbnail: "images/evan/thumbnails/evan-side-6.jpg",
+                description: "Sid:E"
+            },
+
+
+        ],
+        },
+
+"EVAN Covers": {
+        "Covers": [
+            { 
+                name: "[Cover] EVAN Gray and Blue", 
+                url: "https://www.youtube.com/watch?v=uDCONRmyjds", 
+                thumbnail: "images/evan/thumbnails/evan-cover.jpg",
+                description: "Tokyo Tap:E"
+            },
+        ],
+        },
+
+"Debut Showcase": {
+        "DEATH OF ME Debut Showcase": [
+            { 
+                name: "EVAN Debut Showcase DEATH OF ME", 
+                url: "https://www.youtube.com/watch?v=Dp_c_4cTAhc&t=5008s", 
+                thumbnail: "images/evan/thumbnails/evan-showcase.jpg",
+                description: "Debut Showcase"
+            },
+        ],
+        },
+
+
+"Debut Documentary": {
         "About EVAN": [
             { 
                 name: "EVAN Debut Documentary", 
@@ -81,7 +202,50 @@ const categoriesConfig = {
                 url: "https://www.youtube.com/watch?v=3CXQID6rLgk", 
                 thumbnail: "images/evan/thumbnails/evan-itslive.jpg",
                 description: "It's Live New Artist"
-            }
+            },
+{ 
+                name: "EVAN Evan Nail Shop Grand Opening", 
+                url: "https://www.youtube.com/watch?v=DRaXqzVknt0", 
+                thumbnail: "images/evan/thumbnails/evan-nail.jpg",
+                description: "Creator Pick"
+            },
+{ 
+                name: "EVAN Sings Ne-Yo, SZA, Frank Ocean & More", 
+                url: "https://www.youtube.com/watch?v=3sGJ6TZTTLs", 
+                thumbnail: "images/evan/thumbnails/evan-tv.jpg",
+                description: "Teen Vogue"
+            },
+{ 
+                name: "Why do I feel like I have no home? Evan, breaking down on the way to the dorm", 
+                url: "https://www.youtube.com/watch?v=caROXcOcL1E", 
+                thumbnail: "images/evan/thumbnails/evan-stan.jpg",
+                description: "EVAN STAN A"
+            },
+{ 
+                name: "How the Hexagonal Student Became a Pentagon | Ep. 56 EVAN | Practice Room Bada Teacher", 
+                url: "https://www.youtube.com/watch?v=KlHXfJ_QAw0", 
+                thumbnail: "images/evan/thumbnails/evan-bada.jpg",
+                description: "Practice Room"
+            },
+{ 
+                name: "EVAN Death of Me | STAGE WE MAKE | 4K", 
+                url: "https://www.youtube.com/watch?v=DrBKrLNJj_A", 
+                thumbnail: "images/evan/thumbnails/evan-swm.jpg",
+                description: "Stage We Make"
+            },
+{ 
+                name: "EVAN Death of Me Official Performance Video", 
+                url: "https://www.youtube.com/watch?v=E0JO9AaJHRE", 
+                thumbnail: "images/evan/thumbnails/evan-dom-pv.jpg",
+                description: "Performance Video"
+            },
+{ 
+                name: "EVAN Death of Me (Live Performance) | Open Mic", 
+                url: "https://www.youtube.com/watch?v=1IQ8oBdyzCg", 
+                thumbnail: "images/evan/thumbnails/evan-open.jpg",
+                description: "Genius Open Mic"
+            },
+
         ],
         },
     };
@@ -90,11 +254,11 @@ const categoriesConfig = {
 const CONFIG = {
     banners: [
         {
-            title: "EVAN RIDE OR DIE",
-            description: "A new chapter begins. The first solo release is out.",
+            title: "EVAN DEATH OF ME",
+            description: "A new chapter begins. The first mini album is out.",
             background: "images/evan/evan-banner-1.jpg",
             buttonText: "▶ Watch Now",
-            youtubeId: "https://www.youtube.com/watch?v=CsGENgLpyaw",
+            youtubeId: "https://www.youtube.com/watch?v=iNMvUYjaieA",
             type: "image"
         },
         {
